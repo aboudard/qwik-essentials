ce86a0a0e97e8477f62eb620e428af1e9fdaeeb3
Alain BOUDARD <aboudard@connectiv-it.com>
2023-06-13 18:04:04 +0200

    fix: config docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aboudard', // Usually your GitHub org/user name.
+  projectName: 'qwik-essentials', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
